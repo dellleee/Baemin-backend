8de1ff653944f4cfd9612b587bcd8d7be26e483b
minseo <gwonm305@gmail.com>
2024-08-11 16:23:45 +0900

    Swagger 설정

---
+++ back/src/main/java/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("배달의민족 clone API명세서")
+                .description("Delivery 서비스 API 명세서")
+                .version("1.0.0");
+    }
+}
